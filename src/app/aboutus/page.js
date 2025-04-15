eaa9c3bac92a5471fd08fcf89c96aa37468bb364
shayan <shayan.shoeibzadeh69@gmail.com>
2025-04-15 18:46:14 +0300

    change

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function page() {
   return (
     <>
-  <section className=' w-screen h-[100%] absolute bg-[#2a2723]'>
+  <section className=' w-screen h-[100%] absolute bg-[#0E1c1c]'>
     <header className="flex flex-row  w-[100%] justify-center  absolute">
       <div className="flex  justify-start  items-center ml-[2%] gap-10 w-screen">
         <div className=" cursor-pointer rounded-full   text-[#a7a8aa] p-6 hover:underline">
@@ -25,21 +25,22 @@ function page() {
     </div>
     </header>   
   </section>
-  <section className='top-40 flex flex-col w-[100%] relative'>
+  <section className='top-28 flex flex-col w-[100%] relative bg-[#0E1c1c]'>
     <div className='
-    md:w-[100%] lg:w-[90%] md:h-[60vh] lg-[70vh] bg-slate-50/10 z-10 relative  flex self-center
-    text-center  items-center flex-col justify-evenly rounded-md'>
-     <div className=' flex flex-col justify-evenly h-[50%]'> 
-      <p className=''>Welcome to Vulpine</p>
-      <h1 className='text-5xl'>
+    md:w-[100%] lg:w-[90%] md:h-[60vh] lg:h-[80vh] bg-slate-50/10 z-10 relative  flex self-center
+    text-center  items-center flex-col justify-evenly rounded-md text-black '>
+      <Image alt='about-bg' fill="true" className='rounded-lg opacity-50' src="/about-bg.jpg"></Image>
+     <div className=' flex flex-col justify-evenly h-[50%] z-10 '> 
+      <p className='lg:text-xl tracking-widest'>Welcome to Vulpine</p>
+      <h1 className='text-5xl lg:text-7xl tracking-widest'>
         BEST FAMOUS COCKTAILS
       </h1>
-      <p className='px-[10vh]'>
+      <p className='px-[10vh] font-medium lg:text-xl lg:px-[40vh]'>
         these is supposed to be the best bartending service in the entire turkey with not competores.
         you can read more about them in the page below
       </p>
      </div>
-     <div>
+     <div className='z-10'>
         <button className='border p-3 rounded-md'>
           Learn More
         </button>
@@ -50,26 +51,32 @@ function page() {
        >
          ABOUT US </h1>
     </div>
-    <div className='flex flex-row md:w-[100vh] lg:w-[190vh] h-[50vh]  gap-2 self-center
-     bg-slate-500/20  '>
+    <div className='flex flex-row md:w-[100vh] lg:w-[90%] h-[40vh] lg:h-[60vh] gap-2 self-center justify-between
+       mt-5 rounded-md'>
       <div className=' relative w-[33%] '>
-          <Image alt='Lp' fill="true" src="/bar.jpg">
+          <Image alt='Lp' fill="true" src="/negroni.jpg" className='rounded-md'>
 
           </Image>
       </div>
-      <div className='flex flex-col w-[33%] justify-center bg-black'>
-          <p className='text-1xl'>
+      <div className='flex flex-col w-[32%] rounded-md justify-evenly text-center bg-[#29343D]'>
+          <h1 className='lg:text-6xl text-4xl font-normal '>
+            TRADITIONAL & MODERN
+          </h1>
+          <p className='text-1xl lg:text-2xl font-light px-2 lg:px-4'>
               this is just a test to see if you read more to get to know our team using the section made by me in this website
           </p>
-          <button className='p-3 border rounded-md'>
+          <button className='border rounded-md border-black'>
               ReadMore
           </button>
       </div>
       <div className='relative w-[33%]'>
-          <Image alt='Rp' width={100} height={100} src="/bar.jpg">
+          <Image alt='Rp' fill="true" src="/signature.jpg" className='rounded-md' >
 
           </Image>
       </div>
+    </div>
+    <div className='flex flex-row w-[100%] h-[90vh] bg-[#29343D] mt-60'>
+
     </div>
   </section>
 
