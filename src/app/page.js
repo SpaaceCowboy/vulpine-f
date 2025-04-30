7788f8b42a389fd033d1007dd219032f1013732e
shayan <shayan.shoeibzadeh69@gmail.com>
2025-04-30 03:56:15 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -80,7 +80,7 @@ export default function Home() {
     </Navbar>
     <main className="flex flex-col items-center justify-center  h-[100vh] overflow-hidden ">
       <div className="uppercase text-white tracking-[1.rem] md:p-[2rem] lg:p-[0rem] p-[0.5rem] text-[2rem]">
-        <h1 className="lg:text-[4rem] md:text-[2rem] text-center lg:tracking-[0.6rem] text-[2rem]">100 suck</h1>
+        <h1 className="lg:text-[4rem] md:text-[2rem] text-center lg:tracking-[0.6rem] text-[2rem]">Crafting moments one drink at a time</h1>
 
       </div>
         <video
