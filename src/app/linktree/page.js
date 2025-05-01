f533d0bb9301066335988398f61f92a7882d1ef2
shayan <shayan.shoeibzadeh69@gmail.com>
2025-05-01 23:25:17 +0300

    ss

--- src/app/linktree/page.js
+++ src/app/linktree/page.js
@@ -7,7 +7,7 @@ export default function Linktree() {
     return (
         <section className="flex justify-center" >
             <div className="relative w-[390px] h-[100vh] justify-center flex" >
-                <Image alt="linktree-bg" fill={true} className="rounded-lg" src="/linktree.jpg" />
+                <Image alt="linktree-bg" fill={true}  src="/linktree.jpg" />
                 <div className="flex flex-col justify-center gap-20">
                 <button className="z-10 text-white rounded-[50] px-20 py-4 bg-[#39393f] bg-opacity-60 tracking-widest font-medium">
                     INSTGRAM</button>
