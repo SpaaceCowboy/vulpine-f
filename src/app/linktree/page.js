ae7d5c1b54f9791ff21e0341f80d2521625072bb
shayan <shayan.shoeibzadeh69@gmail.com>
2025-05-02 19:18:57 +0300

    linktree

--- src/app/linktree/page.js
+++ src/app/linktree/page.js
@@ -6,18 +6,31 @@ import Link from "next/link";
 export default function Linktree() {
     return (
         <section className="flex justify-center h-[100%]" >
-            <div className="relative w-[390px] h-[100vh] justify-center flex" >
+            <div className="relative w-[390px] h-[100vh] justify-evenly flex flex-col items-center " >
                 <Image alt="linktree-bg" fill={true}  src="/linktree.jpg" />
-                <div className="flex flex-col justify-center gap-20">
-                <button className="z-10 text-white rounded-[50] px-20 py-4 bg-[#39393f] bg-opacity-60 tracking-widest font-medium">
-                    INSTGRAM</button>
-                <button className="z-10 text-white">VULPINE</button>
-                <button className="z-10 text-white">YOUTUBE</button>
-                <button className="z-10 text-white">WHATSAPP</button>
+                <div className="flex flex-col justify-center align-middle  gap-12 w-[80%] ">
+                <Link href="/" className="z-10 ">
+                <button className="z-10 text-white w-[100%] rounded-[50] py-4 bg-[#39393f] bg-opacity-60 tracking-widest font-medium">
+                    INSTGRAM</button> </Link>
+                    <Link href="/" className="z-10 ">
+                <button className="z-10 text-white w-[100%] rounded-[50] py-4 bg-[#39393f] bg-opacity-60 tracking-widest font-medium">
+                    WEBSITE</button> </Link>
+                    <Link href="/" className="z-10 ">
+                <button className="z-10 text-white w-[100%] rounded-[50] py-4 bg-[#39393f] bg-opacity-60 tracking-widest font-medium">
+                    YOUTUBE</button> </Link>
+                    <Link href="/" className="z-10 ">
+                <button className="z-10 text-white w-[100%] rounded-[50] py-4 bg-[#39393f] bg-opacity-60 tracking-widest font-medium">
+                     WHATSAPP</button> </Link>
                 </div>
 
-
+                <div className="z-10 text-center">
+                    <h1 className="text-white text-3xl px-10 ">FOR AN UNFORGETABLE NIGHT</h1>
+                    <p className="text-white text-xl pt-20">Vulpine</p>
+                </div>
             </div>
         </section>
     )
+
+
+
 }
